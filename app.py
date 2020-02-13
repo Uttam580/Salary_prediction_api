bebb95475ae4787cffaee0a08325d05c677f5211
Uttam Kumar <55585930+Uttam580@users.noreply.github.com>
2020-02-13 12:22:47 -0800

    Update app.py

--- app.py
+++ app.py
@@ -9,19 +9,19 @@ model = pickle.load(open('model.pkl', 'rb'))
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['POST'])#post method ,give feature to model.pkl file ,model take input and give output.
 def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    int_features = [int(x) for x in request.form.values()]# take input from all the form  and store into feature 
+    final_features = [np.array(int_features)]# converting into array
+    prediction = model.predict(final_features)# make prediction
 
-    output = round(prediction[0], 2)
+    output = round(prediction[0], 2)# retrieve output
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))# agin rendering index.html and will data
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)# main function to render whole flask
